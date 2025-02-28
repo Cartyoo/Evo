1f5a667c150fd7806d6187e7ff19df77d7f44914
herberto <herberto@herberto.xyz>
2025-02-27 19:54:53 -0800

    Initial commit

---
+++ src/main/java/services/plasma/evo/Evo.java
@@ -0,0 +1,17 @@
+package services.plasma.evo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Evo extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
